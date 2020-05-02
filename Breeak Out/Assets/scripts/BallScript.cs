f693b67ad23eb69e830d932bfe6dc6ed2c93f604
Rakib Hasan Bul Bul <hrakib182@gmail.com>
2020-05-02 13:46:55 +0600

    Dev Log #2
    Ball added

--- Breeak Out/Assets/scripts/BallScript.cs
+++ Breeak Out/Assets/scripts/BallScript.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
+    public Rigidbody2D rb;
+
+    public bool inPlay;
+    public Transform paddle;
+
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D> ();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!inPlay)
+        {
+            transform.position = paddle.position;
+
+        }
+
+        if (Input.GetButtonDown("Jump") && !inPlay)
+        {
+            inPlay = true;
+            rb.AddForce(Vector2.up * speed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other. CompareTag("bottom"))
+        {
+            Debug.Log ("Ball hit bottom");
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+        }
+
     }
 }
